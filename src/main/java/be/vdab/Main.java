35361290ce28ae8fb0ae09dd65a0a5754f494347
Lisa <lisa.m.m.steendam@gmail.com>
2014-05-08 16:23:56 +0200

    Toevoegen dao.xml en services. xml, verwijderen container.xml

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -1,16 +1,27 @@
 package be.vdab;
 
+import java.util.*;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import be.vdab.services.NamenService;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Spring container configuratie begint.");
 		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				"spring/container.xml")) {
-			ClassA objectA = context.getBean(ClassA.class);
-			System.out.println(objectA.getWebMasterEMailAdres());
-			System.out.println(objectA.getWebSiteGestart());
+				"spring/dao.xml", "spring/services.xml")) {
+			List<String> namen = new ArrayList<>();
+			@SuppressWarnings("resource")
+			Scanner scanner = new Scanner(System.in);
+			System.out
+					.println("Tik de namen, gescheiden door Enter. Tik stop na de laatste naam");
+			for (String naam; !"stop".equals(naam = scanner.nextLine());) {
+				namen.add(naam);
+			}
+			NamenService namenService = context.getBean(NamenService.class);
+			namenService.opslaan(namen);
 		}
 	}
 }
